1084babcfccddb5be915b8de020121f2dfc7b1f5
deokhong <igetcool@naver.com>
2019-06-27 11:20:09 +0900

    zuul

--- dining-eureka/src/main/java/com/dining/eureka/server/ServerEurekaApplication.java
+++ dining-eureka/src/main/java/com/dining/eureka/server/ServerEurekaApplication.java
@@ -3,9 +3,11 @@ package com.dining.eureka.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaServer
 public class ServerEurekaApplication {
